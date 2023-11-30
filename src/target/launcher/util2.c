9e7265677c17139893803ca408a5cdd26b08dfbc
Heel <heel@example.com>
2023-11-30 17:34:13 +0900

    bring in isofs code from host

--- src/target/launcher/util2.c
+++ src/target/launcher/util2.c
@@ -1,52 +1,20 @@
 #include <stdarg.h>
+#include <stdint.h>
 
 #include "video.h"
 #include "util.h"
+#include "util2.h"
 
 /* Dummy stub to make libgcc happy... */
 void atexit() { }
 
-unsigned short flash_crc(char *buf, int cnt)
-{
-  static unsigned short table[] = {
-    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-    0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
-    0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
-    0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
-    0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
-    0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
-    0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
-    0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
-    0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
-    0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
-    0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
-    0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
-    0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
-    0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
-    0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
-    0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
-    0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
-    0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
-    0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
-    0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
-    0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
-    0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
-    0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
-    0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
-    0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
-    0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
-    0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
-    0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
-    0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
-    0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
-    0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
-    0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
-  };
-  unsigned short tmp = ~0;
-  while(cnt--)
-    tmp = (tmp<<8)^table[(tmp>>8)^(unsigned char)*buf++];
-  return ~tmp;
-}
+/* Expect to find root directory within this many sectors from the start */
+#define ROOT_DIRECTORY_HORIZON 500
+
+struct isofs_s {
+  unsigned char *data;
+  uint32_t root_lba, root_length;
+};
 
 static int do_plot_string(int x, int y, char *p, unsigned short col)
 {
@@ -92,7 +60,7 @@ static char *format_num(int n, int base, int w, int pad)
   return p;
 }
 
-void vprintf(char *fmt, va_list va)
+void _vprintf(char *fmt, va_list va)
 {
   static int x=8, y=8;
   int w=-1, pad=-1;
@@ -164,6 +132,114 @@ void printf(char *fmt, ...)
 {
   va_list va;
   va_start(va, fmt);
-  vprintf(fmt, va);
+  _vprintf(fmt, va);
   va_end(va);
+}
+
+int strlen(const char *str) {
+    const char *s;
+    for (s = str; *s; ++s);
+    return (s - str);
+}
+
+char to_upper(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return ch - 'a' + 'A';
+    }
+    return ch;
+}
+
+static int ntohlp(const unsigned char *ptr)
+{
+  return (ptr[0]<<24)|(ptr[1]<<16)|(ptr[2]<<8)|ptr[3];
+}
+
+static int fncompare(const char *fn1, int fn1len, const char *fn2, int fn2len)
+{
+  while(fn2len--)
+    if(!fn1len--)
+      return *fn2 == ';';
+    else if(to_upper((unsigned char)*fn1++) != to_upper((unsigned char)*fn2++))
+      return 0;
+  return fn1len == 0;
+}
+
+static int isofs_find_root_directory(isofs iso)
+{
+  uint32_t sec;
+  unsigned char *buf;
+  for (sec = 16; sec < ROOT_DIRECTORY_HORIZON; sec++) {
+    buf = iso->data+sec;
+    if (!memcmp(buf, "\001CD001", 6))
+      break;
+    else if(!memcmp(buf, "\377CD001", 6))
+      return 0;
+  }
+  if (sec >= ROOT_DIRECTORY_HORIZON) {
+    printf("Unabled to find root sector\n");
+    return 0;
+  }
+  printf("Found PVD at LBA %d", sec);
+  iso->root_lba = ntohlp(buf+156+6)+150;
+  iso->root_length = ntohlp(buf+156+14);
+  printf("Root directory is at LBA %d, length %d bytes",
+	       iso->root_lba, iso->root_length);
+  return 1;
+}
+
+static int isofs_find_entry(isofs iso, const char *entryname,
+			     uint32_t *sector, uint32_t *length, int enlen,
+			     uint32_t dirsec, uint32_t dirlen, int dirflag)
+{
+  uint32_t len;
+  unsigned char *buf;
+  const unsigned char *p;
+  dirflag = (dirflag? 2 : 0);
+  while(dirlen > 0) {
+    buf = iso->data + dirsec;
+    if (dirlen > 2048) {
+      len = 2048;
+      dirlen -= 2048;
+      dirsec++;
+    } else {
+      len = dirlen;
+      dirlen = 0;
+    }
+    for (p=buf; len>0; ) {
+      if(!*p || *p>len)
+	break;
+      if (*p>32 && *p>32+p[32])
+	if ((p[25]&2) == dirflag &&
+	    fncompare(entryname, enlen, (const char *)(p+33), p[32])) {
+	  if (sector)
+	    *sector = ntohlp(p+6)+150;
+	  if (length)
+	    *length = ntohlp(p+14);
+	  return 1;
+	}
+      len -= *p;
+      p += *p;
+    }
+  }
+  return 0;
+}
+
+int isofs_find_file(isofs iso, const char *filename,
+		     uint32_t *sector, uint32_t *length)
+{
+  return isofs_find_entry(iso, filename, sector, length, strlen(filename),
+			  iso->root_lba, iso->root_length, 0);
+}
+
+int isofs_init(isofs iso, unsigned char *data)
+{
+  if (iso) {
+    iso->data = data;
+    if (isofs_find_root_directory(iso)) {
+      return 1;
+    } else
+      printf("No PVD found\n");
+  } else
+    printf("Invalid isofs\n");
+  return 0;
 }
